d98c1dabdfeda1e157a066ea06ba2816790564a6
JordanL1 <jordan.lees1@gmail.com>
2016-11-05 21:52:59 +0000

    Add record to db

---
+++ db.py
@@ -0,0 +1,11 @@
+import sqlite3 as lite
+import sys
+
+def db_add(num, email):
+    conn = lite.connect('numbers.db')
+    cursor = conn.cursor()
+    id = cursor.lastrowid
+
+    add = [(id, num, email, 4)]
+
+    cursor.execute("INSERT INTO numbers VALUES(?,?,?,?)", add)

e3bbf8afd5b21256ef50ac892a4bc922de292597
JordanL1 <jordan.lees1@gmail.com>
2016-11-05 23:38:48 +0000

    Commit changes to db

--- db.py
+++ db.py
@@ -4,10 +4,11 @@ import sys
 def db_add(num, email):
     conn = lite.connect('numbers.db')
     cursor = conn.cursor()
-    id = cursor.lastrowid
 
-    add = [id, num, email, 4]
+    add = [num, email, 4]
 
-    cursor.execute("INSERT INTO numbers VALUES(?,?,?,?)", add)
+    cursor.execute("INSERT INTO numbers (phone, email, sent) VALUES(?,?,?)", add)
 
-    print(cursor.fetchall())
+    conn.commit()
+
+    conn.close()

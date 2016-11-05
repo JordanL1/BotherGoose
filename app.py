1cf4594f70668deb0c7353fb31813699e1a8d496
JordanL1 <jordan.lees1@gmail.com>
2016-11-05 21:52:32 +0000

    Add num and email to db

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, url_for, render_template
+import db
+import sqlite3 as lite
+import sys
 
 app = Flask(__name__)
 
@@ -10,4 +13,7 @@ def home_page():
 def handle_post():
     num = request.form['phoneNumber']
     email = request.form['email']
+
+    db.db_add(num, email)
+
     return render_template('confirmation.html')

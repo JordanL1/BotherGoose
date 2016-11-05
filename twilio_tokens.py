cb4046338cc0b3e2c221af5ff000a114463fa4c3
JordanL1 <jordan.lees1@gmail.com>
2016-11-05 17:48:14 +0000

    Twilio tokens for using API

---
+++ twilio_tokens.py
@@ -0,0 +1,2 @@
+account_sid = "PNfa34860df4da8541ba4b1320a1df18dc" # Your Account SID from www.twilio.com/console
+auth_token  = "be3a90c8d624016ec019ccfdec2fee63"  # Your Auth Token from www.twilio.com/console

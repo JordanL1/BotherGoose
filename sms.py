19f4524231fa5688efff0094437f86c5b33eed70
TristanJP <trisperk@hotmail.com>
2016-11-06 13:18:37 +0000

    no message

--- sms.py
+++ sms.py
@@ -5,12 +5,10 @@ class sms_out():
     client = TwilioRestClient(twilio_tokens.account_sid,
                               twilio_tokens.auth_token)
     twilio_number = '+441423740821'
-
-    def send_sms(self, text, nums):
-        for x in range(1, len(nums)):
-            message = self.client.messages.create(body=text,
-                                                  to=nums[x],  # Replace with your phone number
-                                                  from_= self.twilio_number) # Our Twilio number
-												  
 	def send_sms2(self, text, nums):
 		message = self.client.messages.create(body=text, to=nums, from_= self.twilio_number) # Our Twilio number
+		
+    def send_sms(self, text, nums):
+        for x in range(1, len(nums)):
+            message = self.client.messages.create(body=text, to=nums[x], from_= self.twilio_number); # Our Twilio number
+	

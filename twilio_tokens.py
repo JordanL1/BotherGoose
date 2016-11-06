77c943596339daa3e48220f2b3960e1776144dd3
JordanL1 <jordan.lees1@gmail.com>
2016-11-06 00:55:48 +0000

    fixed incorrect SID

--- twilio_tokens.py
+++ twilio_tokens.py
@@ -1,2 +1,2 @@
-account_sid = "PNfa34860df4da8541ba4b1320a1df18dc" # Your Account SID from www.twilio.com/console
+account_sid = "AC9ebd3c73a920d28747ca0d07f98110a3" # Your Account SID from www.twilio.com/console
 auth_token  = "be3a90c8d624016ec019ccfdec2fee63"  # Your Auth Token from www.twilio.com/console

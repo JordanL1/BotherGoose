3e6d215a40ba24ad3d49b2b2be6a43938f7eb21b
JordanL1 <jordan.lees1@gmail.com>
2016-11-06 13:43:46 +0000

    no message

--- sms.py
+++ sms.py
@@ -6,10 +6,11 @@ class sms_out():
                               twilio_tokens.auth_token)
 							  
     twilio_number = '+441423740821'
-	
-	def send_sms2(self, text, nums):
-		message = self.client.messages.create(body=text, to=nums, from_= self.twilio_number) # Our Twilio number	
+
+    def send_sms2(self, text, nums):
+		message = self.client.messages.create(body=text, to=nums, from_= self.twilio_number) # Our Twilio number
+        
     def send_sms(self, text, nums):
         for x in range(1, len(nums)):
-            message = self.client.messages.create(body=text, to=nums[x], from_= self.twilio_number); # Our Twilio numberss
+            message = self.client.messages.create(body=text, to=nums[x], from_= self.twilio_number) # Our Twilio numberss
 	

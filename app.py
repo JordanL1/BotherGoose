271f581b03e27cafd27d2af5f16c998e9bd41519
JordanL1 <jordan.lees1@gmail.com>
2016-11-06 12:28:25 +0000

    Implemented send function

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, url_for, render_template
 import sms
 import db
+import send
 import sys
 
 text = "Thanks for signing up with Bother Goose. Stay tuned for some goosey banter!"
 
 app = Flask(__name__)
+sender = send.sender()
+sender.main()
 
 @app.route('/', methods=['GET'])
 def home_page():

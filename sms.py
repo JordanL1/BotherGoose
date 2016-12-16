1e392036f8aee72b1ef48ae4d09d41a4f7b2c63f
JordanL1 <jordan.lees1@gmail.com>
2016-12-16 11:52:15 +0000

    Documented

--- sms.py
+++ sms.py
@@ -2,12 +2,15 @@ from twilio.rest import TwilioRestClient
 import twilio_tokens
 
 class sms_out():
+    """Handle the sending of SMS."""
     client = TwilioRestClient(twilio_tokens.account_sid,
                               twilio_tokens.auth_token)
 
-    def send_sms2(self, text, nums):
-        message = self.client.messages.create(body=text, to=nums, from_= '+441423740821') # Our Twilio number
+    def send_sms_single(self, text, num):
+        """Send a single SMS to given number."""
+        message = self.client.messages.create(body=text, to=num, from_= '+441423740821') # Our Twilio number
 
     def send_sms(self, text, nums):
+        """Send an SMS to a list of numbers."""
         for x in range(1, len(nums)):
-            message = self.client.messages.create(body=text, to=nums[x], from_= '+441423740821') # Our Twilio numberss
+            message = self.client.messages.create(body=text, to=nums[x], from_= '+441423740821') # Our Twilio numbers
